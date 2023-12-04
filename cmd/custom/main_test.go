ecca914346b5cd46db7b6d7ffd16de9598bd94cd
Nikita Rusin <rusinikit@gmail.com>
2023-12-04 22:58:51 +0200

    feat(ci): use git package for history checks

--- cmd/custom/main_test.go
+++ cmd/custom/main_test.go
@@ -2,204 +2,163 @@ package main
 
 import (
 	"errors"
-	"fmt"
-	"strconv"
 	"strings"
 	"testing"
 
-	"github.com/go-git/go-billy/v5/memfs"
-	"github.com/go-git/go-git/v5"
-	"github.com/go-git/go-git/v5/plumbing"
-	"github.com/go-git/go-git/v5/plumbing/object"
-	"github.com/go-git/go-git/v5/storage/memory"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
+
+	"github.com/rusinikita/changes/git"
 )
 
-var long = strings.Repeat("long ", 20)
+var (
+	longTitle    = strings.Repeat("long ", 20)
+	largeContent = strings.Repeat("row\n", maxFileChangesLen+1)
+)
 
-func Test_check(t *testing.T) {
-	t.Run("ok", func(t *testing.T) {
-		err := check(commit("normal"), nil)
-
-		assert.NoError(t, err)
-	})
-
-	t.Run("long title", func(t *testing.T) {
-		err := check(commit(long), nil)
-
-		assert.Error(t, err)
-	})
-
-	t.Run("long title", func(t *testing.T) {
-		c := commit(long)
-		c.Author.Email = "test@yahoo.com"
-
-		err := check(c, nil)
-
-		assert.Error(t, err)
-	})
-
-	t.Run("long body", func(t *testing.T) {
-		err := check(commit("title\n"+long), nil)
-
-		assert.NoError(t, err)
-	})
-
-	t.Run("skip merge", func(t *testing.T) {
-		longM := "Merge" + long
-
-		err := check(commit(longM), nil)
-
-		assert.NoError(t, err)
-	})
-
+func Test_checkDiff(t *testing.T) {
 	t.Run("many changes", func(t *testing.T) {
-		stats := object.FileStats{object.FileStat{
-			Name:     "test.go",
-			Addition: 401,
+		diff := []git.FileChange{{
+			Path: "test.go",
+			Chunks: []git.Chunk{{
+				Type:    git.Add,
+				Content: largeContent,
+			}},
 		}}
 
-		err := check(commit("normal"), stats)
+		err := checkDiff(diff)
 
 		assert.Error(t, err)
 	})
 
 	t.Run("skip no go changes", func(t *testing.T) {
-		stats := object.FileStats{object.FileStat{
-			Name:     "test",
-			Addition: 401,
+		diff := []git.FileChange{{
+			Path: "test",
+			Chunks: []git.Chunk{{
+				Type:    git.Add,
+				Content: largeContent,
+			}},
 		}}
 
-		err := check(commit("normal"), stats)
+		err := checkDiff(diff)
 
 		assert.NoError(t, err)
 	})
 }
 
-func Test_repositoryChecks(t *testing.T) {
-	t.Run("ok, more 10", func(t *testing.T) {
-		rep, err := git.Init(memory.NewStorage(), memfs.New())
-		require.NoError(t, err)
+func Test_checkCommit(t *testing.T) {
+	t.Run("ok", func(t *testing.T) {
+		err := checkCommit(commit("normal"))
 
-		commits := append([]string{long}, stringRange(0, 10)...)
-		fillCommits(commits, t, rep)
-
-		err = repositoryChecks(rep)
 		assert.NoError(t, err)
 	})
 
-	t.Run("long found", func(t *testing.T) {
-		rep, err := git.Init(memory.NewStorage(), memfs.New())
-		require.NoError(t, err)
-
-		commits := append([]string{long}, stringRange(0, 5)...)
-		fillCommits(commits, t, rep)
-
-		err = repositoryChecks(rep)
+	t.Run("long title", func(t *testing.T) {
+		err := checkCommit(commit(longTitle))
 
 		assert.Error(t, err)
 	})
 
-	t.Run("log error", func(t *testing.T) {
-		rep := repMock{
-			err: errors.New("test"),
-		}
-
-		err := repositoryChecks(rep)
+	t.Run("long title", func(t *testing.T) {
+		err := checkCommit(commit(longTitle))
 
 		assert.Error(t, err)
 	})
 
-	t.Run("log error", func(t *testing.T) {
-		rep := repMock{t: t}
+	t.Run("long body", func(t *testing.T) {
+		err := checkCommit(commit("title\n" + longTitle))
 
-		err := repositoryChecks(rep)
+		assert.NoError(t, err)
+	})
+
+	t.Run("skip merge", func(t *testing.T) {
+		longM := "Merge" + longTitle
+
+		err := checkCommit(commit(longM))
+
+		assert.NoError(t, err)
+	})
+
+	t.Run("email check", func(t *testing.T) {
+		c := commit("normal")
+		c.Author.Email = "test@test.in"
+
+		err := checkCommit(c)
 
 		assert.Error(t, err)
 	})
 }
 
-func commit(message string) *object.Commit {
-	return &object.Commit{
+func Test_checkChange(t *testing.T) {
+	testErr := errors.New("test")
+
+	t.Run("empty ok", func(t *testing.T) {
+		m := changeMock{}
+
+		assert.NoError(t, changesCheck(m))
+	})
+
+	t.Run("commits err", func(t *testing.T) {
+		m := changeMock{diffErr: testErr}
+
+		assert.Error(t, changesCheck(m))
+	})
+
+	t.Run("diff err", func(t *testing.T) {
+		m := changeMock{commitsErr: testErr}
+
+		assert.Error(t, changesCheck(m))
+	})
+
+	t.Run("commits check", func(t *testing.T) {
+		m := changeMock{commits: []git.Commit{{
+			Message: longTitle,
+		}}}
+
+		assert.Error(t, changesCheck(m))
+	})
+
+	t.Run("diff check", func(t *testing.T) {
+		m := changeMock{diff: []git.FileChange{{
+			Path: "test.go",
+			Chunks: []git.Chunk{{
+				Type:    git.Add,
+				Content: largeContent,
+			}},
+		}}}
+
+		assert.Error(t, changesCheck(m))
+	})
+}
+
+func commit(message string) git.Commit {
+	return git.Commit{
 		Message: message,
-		Author: object.Signature{
+		Author: git.Signature{
 			Name:  "Test",
 			Email: "test@gmail.com",
 		},
 	}
 }
 
-func fillCommits(cc []string, t *testing.T, r *git.Repository) {
-	wt, err := r.Worktree()
-	require.NoError(t, err)
-
-	for _, commit := range cc {
-		fileName := fmt.Sprintf("%s.txt", commit)
-		file, err := wt.Filesystem.Create(fileName)
-		require.NoError(t, err)
-
-		_, err = file.Write([]byte(commit))
-		require.NoError(t, err)
-		require.NoError(t, file.Close())
-
-		_, err = wt.Add(fileName)
-		require.NoError(t, err)
-		_, err = wt.Commit(commit, &git.CommitOptions{
-			Author: &object.Signature{
-				Name:  commit,
-				Email: "test@gmail.com",
-			},
-			Committer: &object.Signature{
-				Name:  commit,
-				Email: "test@gmail.com",
-			},
-		})
-		require.NoError(t, err)
-	}
-}
-
-func stringRange(f, l int) []string {
-	result := make([]string, 0, l-f)
-
-	for i := f; i <= l; i++ {
-		result = append(result, strconv.Itoa(i))
-	}
-
-	return result
-}
-
 func Test_main(_ *testing.T) {
 	_ = run()
 }
 
-type repMock struct {
-	t   *testing.T
-	err error
+type changeMock struct {
+	commits    []git.Commit
+	commitsErr error
+	diff       []git.FileChange
+	diffErr    error
 }
 
-func (repMock) Next() (*object.Commit, error) {
-	return nil, errors.New("implement me")
+func (changeMock) Branches() (source, target string) {
+	return "", ""
 }
 
-func (r repMock) ForEach(f func(*object.Commit) error) error {
-	rep, err := git.Init(memory.NewStorage(), memfs.New())
-	assert.NoError(r.t, err)
-
-	fillCommits([]string{"test"}, r.t, rep)
-
-	iter, err := rep.Log(&git.LogOptions{})
-	assert.NoError(r.t, err)
-
-	return iter.ForEach(func(c *object.Commit) error {
-		c.TreeHash = plumbing.NewHash("")
-
-		return f(c)
-	})
+func (c changeMock) Commits() ([]git.Commit, error) {
+	return c.commits, c.commitsErr
 }
 
-func (repMock) Close() {}
-
-func (r repMock) Log(_ *git.LogOptions) (object.CommitIter, error) {
-	return r, r.err
+func (c changeMock) FilesDiff() ([]git.FileChange, error) {
+	return c.diff, c.diffErr
 }
