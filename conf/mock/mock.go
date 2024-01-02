91b0511bde3ced362e8e7d43ae0785cce3636c51
Nikita Rusin <rusinikit@gmail.com>
2024-01-02 22:37:28 +0800

    fix: mock generation

--- conf/mock/mock.go
+++ conf/mock/mock.go
@@ -1,25 +1,8 @@
 package mock
 
 import (
-	"reflect"
-
 	"github.com/rusinikita/changes/conf"
 )
 
 //go:generate moq -rm -stub -out conf_mock.go . Conf
 type Conf = conf.Conf
-
-func NewUnmarshal(value any, err error) *ConfMock {
-	return &ConfMock{
-		UnmarshalFunc: func(key string, config any, defaultValue ...any) error {
-			if err != nil {
-				return err
-			}
-
-			rValue := reflect.ValueOf(config)
-			rValue.Elem().Set(reflect.ValueOf(value))
-
-			return nil
-		},
-	}
-}
